150598dbb50065cbe5596b8c9777846f5d204995
luckymogale64 <mogalel615@gmail.com>
2022-08-15 08:55:03 +0200

    update src

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -6,9 +6,7 @@ export default function Posts() {
     <div className="posts">
       <Post img="https://images.pexels.com/photos/1194420/pexels-photo-1194420.jpeg?auto=compress&cs=tinysrgb&w=600" />
       <Post img="https://images.pexels.com/photos/775201/pexels-photo-775201.jpeg?auto=compress&cs=tinysrgb&w=800" />
-      <Post img="https://images.pexels.com/photos/6711867/pexels-photo-6711867.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
-      <Post img="https://images.pexels.com/photos/5490778/pexels-photo-5490778.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
-      <Post img="https://images.pexels.com/photos/4916559/pexels-photo-4916559.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
+      
     </div>
   );
 }
